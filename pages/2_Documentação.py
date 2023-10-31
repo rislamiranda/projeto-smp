0bca5a33acc57b1a3ab932ac3f13061b5ffc3b8a
JaneAdriana88 <148550681+JaneAdriana88@users.noreply.github.com>
2023-10-31 16:37:23 -0300

    Pagina WEB atualizada Quem Somos

--- pages/2_Documentação.py
+++ pages/2_Documentação.py
@@ -26,3 +26,21 @@ st.markdown("- Criar um relatório final com as análises realizadas e publicaç
 st.markdown("<h5 style='text-align: justify; color: black;'>A documentação complementar desse projeto pode ser acessada pelos links abaixo especificados:</h5>", unsafe_allow_html=True)
 st.markdown("-------------------")
 
+# Localize o arquivo .pdf
+arquivo_pdf = "./documentacao/monitoramentoFrequenciaEvasaoPresenca.pdf"
+
+
+# Instruções para o usuário
+st.write("Clique no botão abaixo para fazer o download do relatório do projeto no formato PDF.")
+
+with open(arquivo_pdf, "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+
+st.download_button(label="Relatório do Projeto",
+                    data=PDFbyte,
+                    file_name="RelatorioProjeto.pdf",
+                    mime='application/octet-stream')
+
+
+
+
